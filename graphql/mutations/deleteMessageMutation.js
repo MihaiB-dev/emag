7c4c40a62fbb96a98ab5049b5047ad52612424e5
Alezuu <alexandrazugravu3@gmail.com>
2025-01-12 17:02:16 +0200

    Add resolver check if is the user

--- graphql/mutations/deleteMessageMutation.js
+++ graphql/mutations/deleteMessageMutation.js
@@ -1,24 +1,32 @@
 import { GraphQLBoolean, GraphQLInt } from 'graphql';
 import db from '../../models/index.js';
 
-const deleteMessageMutation = {
-  type: GraphQLBoolean, 
-  args: {
-    id: { type: GraphQLInt }, 
-  },
-  resolve: async (_, { id }) => {
-    try {
-      const message = await db.Message.findByPk(id);
-      if (!message) {
-        return false; 
-      }
+const deleteMessageMutationResolver = async (_, { id }, context) => {
+  try {
+    const message = await db.Message.findByPk(id);
 
-      await message.destroy();
-      return true; 
-    } catch (error) {
-      return false; 
+    if (!message) {
+      throw new Error('Message not found');
     }
-  },
+
+    if (message.userId !== context.user_id) {
+      throw new Error('You are not authorized to delete this message');
+    }
+
+    await message.destroy();
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const deleteMessageMutation = {
+    type: GraphQLBoolean, 
+    args: {
+      id: { type: GraphQLInt },
+    },
+    resolve: deleteMessageMutationResolver,
 };
 
 export default deleteMessageMutation;
